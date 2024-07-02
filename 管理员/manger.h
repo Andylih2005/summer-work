29da193ef47b1eb6c2c45bc0b28270758e9301cf
Andylih2005 <145590865+Andylih2005@users.noreply.github.com>
2024-07-02 17:20:32 +0800

    Update and rename manger.h to 管理员/manger.h

---
+++ 管理员/manger.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include<iostream>
+#include"book.h"
+using namespace std;
+class manger
+{
+	string idname;//用户名
+	string password;//密码
+	manger(string id,string pw):idname(id),password(pw){}
+	void addbook(string name,string type,string id,int number,string author,string press);
+	void editbook( string name, string type, string id, int number, string author, string press);
+	void delbook(string name, string type, string id, int number, string author, string press);
+	void changenum(string id, int num);
+};
